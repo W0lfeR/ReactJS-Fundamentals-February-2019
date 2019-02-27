f9d6dff3be7f7dc4cc980dded7f3314d5d4d17a4
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-02-27 13:19:49 +0200

    Add files via upload

--- 3. Events and Forms/src/Games/CreateForm.js
+++ 3. Events and Forms/src/Games/CreateForm.js
@@ -2,19 +2,32 @@ import React from 'react';
 
 const CreateForm = (props) => {
 
+    const state = {
+        title: null,
+        description: null,
+        imageUrl: null
+    }
+
+
+    const handleChange = (event) => (
+        state[event.target.name] = event.target.value
+    )
+
+
     return (
         <div className="create-form">
             <h1>Create game</h1>
             <form onSubmit={(event) => {
-                // TODO: prevent the default behaviour of the click event, call the createGame function and pass it the data from the form
+                event.preventDefault();
+                props.createGame(state);
             }}>
                 <label>Title</label>
-                <input type="text" id="title"/>
+                <input type="text" onChange={handleChange} name="title" id="title" />
                 <label>Description</label>
-                <textarea type="text" id="description"/>
+                <textarea type="text"  onChange={handleChange} name="description" id="description" />
                 <label>ImageUrl</label>
-                <input type="text" id="imageUrl"/>
-                <input type="submit" value="Create"/>
+                <input type="text" onChange={handleChange} name="imageUrl" id="imageUrl" />
+                <input type="submit" value="Create" />
             </form>
         </div>
     )
