513856470b8d27ead4ec02ba24302d91d383cf84
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-03-07 19:34:30 +0200

    Add files via upload

---
+++ skyline-technology-group/src/views/not-found.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <h2>These are not the droids you are looking for</h2>
+        );
+    }
+}
+
+export default NotFound;
