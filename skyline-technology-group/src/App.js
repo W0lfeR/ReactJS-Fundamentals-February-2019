32655ac08a08b954d4373fb6221f7616ff4e7198
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-03-07 19:35:08 +0200

    Add files via upload

--- skyline-technology-group/src/App.js
+++ skyline-technology-group/src/App.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './views/home'
+import NotFound from './views/not-found'
 
 
 class App extends Component {
   render() {
     return (
-    <div></div>
+      <div>
+        <Router>
+          <Fragment>
+            <header>
+              <nav class="navbar-menu"><a href="/">Book Store</a><a href="/" class="active" aria-current="page">Home</a>
+              <a href="/computers">Computers</a>
+                <a href="/orders">My Orders</a>
+                <a href="/cart">Cart</a>
+                <a href="javascript:void(0)">Logout</a>
+                </nav>
+            </header>
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route component={NotFound} />
+            </Switch>
+            <div>
+              <footer id="footer" class="page-footer mt-4">© Skyline Technology Group</footer>
+            </div>
+          </Fragment>
+        </Router>
+      </div>
     );
   }
 }
