cf53483efee731edbbd8d027fc17138951cb1b99
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-12-19 15:51:51 +0200

    Add files via upload

--- socialsystemblog/testhome/src/components/navbar/Navbar.jsx
+++ socialsystemblog/testhome/src/components/navbar/Navbar.jsx
@@ -5,35 +5,25 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = props => {
     const { loggedIn, isAdmin, logout, } = props;
-    const userId = localStorage.getItem("userId");
-  
-    return (
 
+    return (
         <div className="bs-component">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarColor02">
-                <ul className="navbar-nav mr-auto">
-                <NavLink className="nav-link-right" to="/">Home</NavLink>
-                {isAdmin && (
-              <NavLink className="nav-link-right" to="/create"> </NavLink>
-            )}
-            {!loggedIn && (
-              <NavLink className="nav-link-right" to="/login">Login</NavLink>
-            )}
-            {!loggedIn && (
-              <NavLink className="nav-link-right" to="/register">Register</NavLink>
-            )}
-            {loggedIn && (
-              <a href="/" className="nav-link-right" onClick={logout}>Logout</a>
-            )}
+            <ul className="navbar-nav mr-auto">
+            <NavLink className="nav-link-right" to="/">Home</NavLink>
+            <NavLink className="nav-link-right" to="/products">Products</NavLink>
+            {isAdmin && (<NavLink className="nav-link-right" to="/product/create">New</NavLink> )}
+            {!loggedIn && (<NavLink className="nav-link-right" to="/login">Login</NavLink>)}
+            {!loggedIn && (<NavLink className="nav-link-right" to="/register">Register</NavLink>)}
+            {loggedIn && ( <a href="/" className="nav-link-right" onClick={logout}>Logout</a> )}
                 </ul>
             </div>
         </nav>
     </div>
-
     )
 }
 
